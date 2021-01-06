8dde1484e05dde7324215b7a9f84b9dcf86c26c7
Henry Leung <henryskyleung@gmail.com>
2021-01-06 12:58:18 -0500

    fix linux compilation and add zlib

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include "tiffio.h"
+#include "zlib.h"
 
-int main(int, char**) {
+int main(int argc, char* argv[]) {
     std::cout << "libtiff version:" << TIFFLIB_VERSION << "\n";
+    std::cout << "zlib version:" << ZLIB_VERSION << "\n";
+    TIFF* tif = TIFFOpen(argv[1], "r");
+    if (tif) {
+	int dircount = 0;
+	do {
+	    dircount++;
+	} while (TIFFReadDirectory(tif));
+	printf("%d directories in %s\n", dircount, argv[1]);
+	TIFFClose(tif);
+    }
     return 0;
 }
